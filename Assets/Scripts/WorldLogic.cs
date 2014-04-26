dd7cd40bf2a343a3193a262555c115547f62d8d9
Joseph Utecht <thenmal@gmail.com>
2014-04-26 18:44:02 -0500

    camera bound to square

--- Assets/Scripts/WorldLogic.cs
+++ Assets/Scripts/WorldLogic.cs
@@ -26,8 +26,10 @@ public class WorldLogic : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButtonDown("Fire1")){
+			Debug.Log("click");
 			// Attempt to set new target
 			int type = wg.ClickToType();
+			Debug.Log(type);
 			Vertex target = wg.ClickToVertex();
 			if(type == WorldGen.CARROT){
 				if(!food_targets.Contains(target)){
