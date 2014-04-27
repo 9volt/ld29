f5cc4678c13a4addcfc60d57eed4e804b3862e7d
Sonya <sonya.utecht@gmail.com>
2014-04-26 22:58:00 -0500

    Bunny Targeting and Proffesion Changing

---
+++ Assets/Scripts/Bunny_Targeter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Bunny_Targeter : MonoBehaviour {
+	public GameObject[] bunnies;
+	private int pos;
+	private GUIScript g;
+
+	// Use this for initialization
+	void Start () {
+		bunnies = GameObject.FindGameObjectsWithTag("Rabbit");
+		pos = 0;
+		g = gameObject.GetComponent<GUIScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bunnies = GameObject.FindGameObjectsWithTag("Rabbit"); //cause rabbits can die whenever
+		if (bunnies.Length <= 0){
+			g.currentRabbit = null;
+		}else{
+			if(Input.GetKeyDown(KeyCode.Tab)){
+				g.currentRabbit.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+				if (pos + 1 <= bunnies.Length - 1){
+					pos++;
+				}else{
+					pos = 0;
+				}
+			}
+			g.currentRabbit = bunnies[pos].GetComponent<RabbitLogic>();
+		}
+
+	}
+	
+}
--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -34,24 +34,39 @@ public class GUIScript : MonoBehaviour {
 //		if (GUI.Button(new Rect(Screen.width - 200, Screen.height - 200, 200,100), "Change name")){
 //			name = gameObject.GetComponent<NameGen>().getName();
 //		}
+		if(currentRabbit != null){
 
-		GUI.Box(new Rect(Screen.width - 200, Screen.height - 100, 200,100), currentRabbit.myname);
-		if(currentRabbit.sex == RabbitLogic.FEMALE){
-			GUI.DrawTexture(new Rect(Screen.width - 55, Screen.height - 100, 30,30), female);
-		}else{
-			GUI.DrawTexture(new Rect(Screen.width - 55, Screen.height - 100, 30,30), male);
+			//InfoBox
+			GUI.Box(new Rect(Screen.width - 200, Screen.height - 100, 200,100), currentRabbit.myname);
+			if(currentRabbit.sex == RabbitLogic.FEMALE){
+				GUI.DrawTexture(new Rect(Screen.width - 55, Screen.height - 100, 30,30), female);
+			}else{
+				GUI.DrawTexture(new Rect(Screen.width - 55, Screen.height - 100, 30,30), male);
+			}
+			//profession icon
+			GUI.DrawTexture(new Rect(Screen.width - 195, Screen.height - 93, 30,30), profession);
+			//stats
+			GUI.Label(new Rect(Screen.width - 200 + profession.width + 10, Screen.height - 80, 50,20), "Spd:" + currentRabbit.spd);
+			GUI.Label(new Rect(Screen.width - 200 + profession.width + 10, Screen.height - 60, 50,20), "Str:" + currentRabbit.str);
+			//hunger and hp bars	
+			GUI.Label(new Rect(Screen.width - 195,Screen.height - 35, 100,20), "Hunger:");
+			GUI.Label(new Rect(Screen.width - 195,Screen.height - 20, 100,20), "HP:");
+			GUI.DrawTexture(new Rect(Screen.width - 145,Screen.height - 30, 100,10), emptytexture);
+			GUI.DrawTexture(new Rect(Screen.width - 145, Screen.height - 30, (float)(currentRabbit.hunger/(float)currentRabbit.full) * 100,10), hungertexture);
+			GUI.DrawTexture(new Rect(Screen.width - 145,Screen.height - 15, 100,10), emptytexture);
+			GUI.DrawTexture(new Rect(Screen.width - 145, Screen.height - 15, (float)(currentRabbit.hp/(float)currentRabbit.maxhp) * 100,10), hptexture);
+			if(GUI.Button(new Rect(Screen.width - 200 + profession.width + 60, Screen.height - 80, 50,20),">")){
+				if(currentRabbit.profession == "Burrower"){
+					currentRabbit.profession = "Forager";
+				}else if(currentRabbit.profession == "Forager"){
+					currentRabbit.profession = "Guard";
+				}else{
+					currentRabbit.profession = "Burrower";
+				}
+			}
+
+			//To indicate your rabbit
+			currentRabbit.gameObject.GetComponent<SpriteRenderer>().color = Color.gray;
 		}
-		//profession icon
-		GUI.DrawTexture(new Rect(Screen.width - 195, Screen.height - 93, 30,30), profession);
-		//stats
-		GUI.Label(new Rect(Screen.width - 200 + profession.width + 10, Screen.height - 80, 50,20), "Spd:" + currentRabbit.spd);
-		GUI.Label(new Rect(Screen.width - 200 + profession.width + 10, Screen.height - 60, 50,20), "Str:" + currentRabbit.str);
-		//hunger and hp bars	
-		GUI.Label(new Rect(Screen.width - 195,Screen.height - 35, 100,20), "Hunger:");
-		GUI.Label(new Rect(Screen.width - 195,Screen.height - 20, 100,20), "HP:");
-		GUI.DrawTexture(new Rect(Screen.width - 145,Screen.height - 30, 100,10), emptytexture);
-		GUI.DrawTexture(new Rect(Screen.width - 145, Screen.height - 30, (float)(currentRabbit.hunger/(float)currentRabbit.full) * 100,10), hungertexture);
-		GUI.DrawTexture(new Rect(Screen.width - 145,Screen.height - 15, 100,10), emptytexture);
-		GUI.DrawTexture(new Rect(Screen.width - 145, Screen.height - 15, (float)(currentRabbit.hp/(float)currentRabbit.maxhp) * 100,10), hptexture);
 	}
 }
