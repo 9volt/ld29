a93318732e2e5f36ffd2111679654e38dcfbefbe
Joseph Utecht <thenmal@gmail.com>
2014-04-29 17:17:50 -0500

    bug fixes with quality settings

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -19,25 +19,25 @@ public class CameraMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		bg.renderer.material.mainTexture = seasons[wl.season];
-		if(Input.mousePosition.x > Screen.width * .9f && camera.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x < (wg.transform.position.x + wg.width - 2)){
+		if((Input.mousePosition.x > Screen.width * .9f || Input.GetAxis("Horizontal") > 0f) && camera.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x < (wg.transform.position.x + wg.width - 2)){
 			//scroll right
-			transform.Translate(Vector3.right * scroll_speed);
+			transform.Translate(Vector3.right * scroll_speed * Time.deltaTime);
 			bg.renderer.material.mainTextureOffset = new Vector2((bg.renderer.material.mainTextureOffset.x + bg_scroll_speed)%1, 0f);
 		}
-		if(Input.mousePosition.x < Screen.width * .1f && camera.ScreenToWorldPoint(new Vector3(0, 0, 0)).x > wg.transform.position.x + 1){
+		if((Input.mousePosition.x < Screen.width * .1f || Input.GetAxis("Horizontal") < 0f) && camera.ScreenToWorldPoint(new Vector3(0, 0, 0)).x > wg.transform.position.x + 1){
 			//scroll left
-			transform.Translate(Vector3.left * scroll_speed);
+			transform.Translate(Vector3.left * scroll_speed * Time.deltaTime);
 			bg.renderer.material.mainTextureOffset = new Vector2( (bg.renderer.material.mainTextureOffset.x + -bg_scroll_speed)%1, 0f);
 		}
-		if(Input.mousePosition.y > Screen.height * .9f && camera.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y < wg.transform.position.y - 2){
+		if((Input.mousePosition.y > Screen.height * .9f || Input.GetAxis("Vertical") > 0f) && camera.ScreenToWorldPoint(new Vector3(0, Screen.height, 0)).y < wg.transform.position.y - 2){
 			//scroll up
-			transform.Translate(Vector3.up * scroll_speed);
+			transform.Translate(Vector3.up * scroll_speed * Time.deltaTime);
 		}
-		if(Input.mousePosition.y < Screen.height * .1f && camera.ScreenToWorldPoint(new Vector3(0, 0, 0)).y > (wg.transform.position.y - wg.height + 2)){
+		if((Input.mousePosition.y < Screen.height * .1f || Input.GetAxis("Vertical") < 0f) && camera.ScreenToWorldPoint(new Vector3(0, 0, 0)).y > (wg.transform.position.y - wg.height + 2)){
 			//scroll down
-			transform.Translate(Vector3.down * scroll_speed);
+			transform.Translate(Vector3.down * scroll_speed * Time.deltaTime);
 		}
-		camera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoom_speed;
+		camera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoom_speed * Time.deltaTime;
 	}
 
 	public void SetPosition(Vertex v){
